54d4bda02c88e53a90306d572449e206de2799a5
trig0n03 <104526558+trig0n03@users.noreply.github.com>
2026-09-25 13:23:57 +0200

    Used Aggregate to calculate the harmonic sum

--- osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
@@ -6,12 +6,13 @@ using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
 using System.Linq;
+using osu.Game.Rulesets.Difficulty.Aggregation;
 
 namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 {
     public class Movement : StrainDecaySkill
     {
-        protected override double SkillMultiplier => 9.3;
+        protected override double SkillMultiplier => 1.9;
         protected override double StrainDecayBase => 0.05;
 
         protected override int SectionLength => 750;
@@ -28,21 +29,14 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 
         public override double DifficultyValue()
         {
-            double difficulty = 0;
-            int shift = 5;
-            int counter = 0;
-
             // Sections with 0 strain are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
             // These sections will not contribute to the difficulty.
-            var peaks = GetCurrentStrainPeaks().Where(p => p > 0);
+            var peaks = GetCurrentStrainPeaks().Where(p => p > 0).ToList();
 
-            // Difficulty is the weighted sum of the highest strains from every section.
-            // We're sorting from highest to lowest strain.
-            foreach (double strain in peaks.OrderDescending())
-            {
-                difficulty += strain / (counter + shift);
-                counter += 1;
-            }
+            if (ObjectDifficulties.Count == 0)
+                return 0;
+
+            (double difficulty, _) = HarmonicSeries.Aggregate(peaks, harmonicScale: 15, decayExponent: 0.7);
 
             return difficulty;
         }
